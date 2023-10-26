7bc1370ba9e8358130ec034ce5524b4c632882a0
zmo2s <anto.96@live.fr>
2023-10-26 10:51:31 +0200

    feat map and login page

--- lib/bottom_navigation_bar.dart
+++ lib/bottom_navigation_bar.dart
@@ -1,6 +1,8 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:plant/login.dart';
 import 'package:plant/pictureList.dart';
+import 'package:plant/take_picture.dart';
 
 import 'map.dart';
 
@@ -23,21 +25,46 @@ class BottomNavigationBarExampleApp extends StatelessWidget {
 class BottomNavigationBarExample extends StatefulWidget {
   const BottomNavigationBarExample({super.key});
 
+
+
   @override
   State<BottomNavigationBarExample> createState() =>
       _BottomNavigationBarExampleState();
+
+
 }
 
 class _BottomNavigationBarExampleState
     extends State<BottomNavigationBarExample> {
+
+  late Future<CameraDescription> cameraFuture;
+  late CameraDescription cam;
+  var firstCamera = null;
+
+   initializeCamera() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // Obtain a list of the available cameras on the device.
+    final cameras = await availableCameras();
+
+    // Get a specific camera from the list of available cameras.
+     firstCamera = cameras.first;
+
+    // You can do something with 'firstCamera' here, like storing it in a state variable.
+  }
+
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
   TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static  List<Widget> _widgetOptions = <Widget>[
 
     MapWidget(),
-    LoginPage()
+    PictureList(),
+    TakePictureScreen(camera: firstCamera)
+
+
   ];
+
 
   void _onItemTapped(int index) {
     setState(() {
